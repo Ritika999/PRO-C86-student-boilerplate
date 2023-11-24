671aaf065f114bf4b963b7888fe0ba3f9d539426
Ritika999 <70030520+Ritika999@users.noreply.github.com>
2023-11-24 13:20:52 +0530

    Update Profile.js

--- screens/Profile.js
+++ screens/Profile.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  Image,
+  Switch
+} from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
-import * as SplashScreen from 'expo-splash-screen';
 import * as Font from "expo-font";
 
+import * as SplashScreen from 'expo-splash-screen';
+
 SplashScreen.preventAutoHideAsync();
+
+import firebase from "firebase";
 
 let customFonts = {
   "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
@@ -14,8 +26,15 @@ export default class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      fontsLoaded: false
+      fontsLoaded: false,
+      isEnabled: false,
+      light_theme: true,
+      name: ""
     };
+  }
+
+  toggleSwitch() {
+   
   }
 
   async _loadFontsAsync() {
@@ -25,6 +44,11 @@ export default class Profile extends Component {
 
   componentDidMount() {
     this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  async fetchUser() {
+
   }
 
   render() {
@@ -32,7 +56,43 @@ export default class Profile extends Component {
       SplashScreen.hideAsync();
       return (
         <View style={styles.container}>
-          <Text>Profile</Text>
+          <SafeAreaView style={styles.droidSafeArea} />
+          
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Storytelling App</Text>
+            </View>
+          </View>
+
+          <View style={styles.screenContainer}>
+            <View style={styles.profileImageContainer}>
+              
+                /* Name of the user*/
+
+
+            </View>
+            <View style={styles.themeContainer}>
+              
+           
+                /* 
+                
+                Theme name
+                Switch 
+                
+                */
+            
+            
+            </View>
+            <View style={{ flex: 0.3 }} />
+          </View>
+       
+          <View style={{ flex: 0.08 }} />
         </View>
       );
     }
@@ -42,7 +102,63 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#15193c"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
     justifyContent: "center",
     alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  screenContainer: {
+    flex: 0.85
+  },
+  profileImageContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  profileImage: {
+    width: RFValue(140),
+    height: RFValue(140),
+    borderRadius: RFValue(70)
+  },
+  nameText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(10)
+  },
+  themeContainer: {
+    flex: 0.2,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: RFValue(20)
+  },
+  themeText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15)
   }
 });
